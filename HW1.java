614f4c5c2cf9c298c95f97d04f629305ab290d7e
aidang116 <aidang116@nycstudents.net>
2025-11-04 11:11:07 -0500

    Created HW1.java

---
+++ HW1.java
@@ -0,0 +1,28 @@
+public class HW1{
+	public static void main(String[] args){
+		System.out.println("PROBLEM 1");
+                //Make(declare) four integer variables a, b, c, and d with values 1, 2, 2, and 5.
+                //Print the following:
+                //The numerator of the first fraction is [PUT VARIABLE a HERE]
+                //The denominator of the first fraction is [PUT VARIABLE b HERE]
+                //The numerator of the second fraction is [PUT VARIABLE c HERE]
+                //The denominator of the second fraction is [PUT VARIABLE d HERE]
+                //Then, print the value of the numerator and denominator of the sum of the two fractions.
+                //The command to print is System.out.println(THING TO PRINT);
+
+                //code goes here
+    
+                System.out.println("PROBLEM 2");
+                //Make a double F with a value of 50.0
+                //Calculate and print its equivalent in Celsius.
+
+                //code goes here
+    
+                System.out.println("PROBLEM 3");
+                //Make an integer array with the values 1, 3, 6, 4, 7, 9, 2, 6, 3 in that order.
+                //Use a for loop and no built-in Math methods to find its max and min. Print these two values.
+	
+                //code goes here
+  
+        }
+}

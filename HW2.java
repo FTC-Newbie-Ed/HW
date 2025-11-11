5c71901f6ccb49c92588a736c0ba33f762f15950
Aidan Gray <aidangray@mac.myfiosgateway.com>
2025-11-11 12:48:11 -0500

    hw2 fixed

---
+++ HW2.java
@@ -0,0 +1,14 @@
+public class HW2 {
+    public void factorial(int n) {
+    //Given n of 1 or more, print the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+    //CODE GOES HERE
+    }
+    public static void main(String[] args) {
+        //Declare the int array arr and print the number of even ints in the array. Do this once with a for loop and once with a while loop.
+        //CODE GOES HERE
+
+        //Declare an array of ints, then print true if the array contains a 2 next to a 2 somewhere. 
+        //Do this twice, once with a for loop and once with a while loop.
+        //CODE GOES HERE
+    }
+}
